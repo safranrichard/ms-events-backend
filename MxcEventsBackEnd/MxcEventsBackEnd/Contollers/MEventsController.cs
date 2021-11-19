6f50ecff4d5fdd991fd4623040bfb9c913e9bb1b
safranrichard <nagyrichike@gmail.com>
2021-11-19 16:53:05 +0100

    Create controller

---
+++ MxcEventsBackEnd/MxcEventsBackEnd/Contollers/MEventsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MxcEventsBackEnd.Models;
+
+namespace MxcEventsBackEnd.Contollers
+{
+    public class MEventsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MEventsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: MEvents
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.MEvents.ToListAsync());
+        }
+
+        // GET: MEvents/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mEvent = await _context.MEvents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mEvent == null)
+            {
+                return NotFound();
+            }
+
+            return View(mEvent);
+        }
+
+        // GET: MEvents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MEvents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name,Location,Country,Capacity,CreationDate,Id")] MEvent mEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                mEvent.Id = Guid.NewGuid();
+                _context.Add(mEvent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mEvent);
+        }
+
+        // GET: MEvents/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mEvent = await _context.MEvents.FindAsync(id);
+            if (mEvent == null)
+            {
+                return NotFound();
+            }
+            return View(mEvent);
+        }
+
+        // POST: MEvents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("Name,Location,Country,Capacity,CreationDate,Id")] MEvent mEvent)
+        {
+            if (id != mEvent.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mEvent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MEventExists(mEvent.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mEvent);
+        }
+
+        // GET: MEvents/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mEvent = await _context.MEvents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (mEvent == null)
+            {
+                return NotFound();
+            }
+
+            return View(mEvent);
+        }
+
+        // POST: MEvents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var mEvent = await _context.MEvents.FindAsync(id);
+            _context.MEvents.Remove(mEvent);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MEventExists(Guid id)
+        {
+            return _context.MEvents.Any(e => e.Id == id);
+        }
+    }
+}

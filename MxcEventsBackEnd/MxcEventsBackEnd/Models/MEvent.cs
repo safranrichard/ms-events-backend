f71e3ead47a05f8f7de31fab368371fec65d0847
safranrichard <nagyrichike@gmail.com>
2021-11-19 16:36:07 +0100

    create models

---
+++ MxcEventsBackEnd/MxcEventsBackEnd/Models/MEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MxcEventsBackEnd.Models
+{
+    public class MEvent
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
+
+        public string Country { get; set; }
+
+        [Range(0, Int32.MaxValue, ErrorMessage = "Value should be greater than or equal to 0")]
+        public int Capacity { get; set; }
+
+        public DateTime CreationDate { get; private set; }
+
+        public Guid Id { get; private set; }
+
+        //setters
+        public void SetCreationDate()
+        {
+            CreationDate = DateTime.Now;
+        }
+
+        public void SetId()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
